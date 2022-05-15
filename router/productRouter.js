ba354adae403bdfc8fd24bfd439dfdecf1efe465
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-05-15 11:11:02 +0545

    find by id

--- router/productRouter.js
+++ router/productRouter.js
@@ -30,5 +30,59 @@ router.post('/addproduct',async(req,res)=>{
   }
 
 })
-router.delete('/deleteProduct',)
+router.delete('/deleteProduct',async(req,res)=>{
+    const product = await Product.findById(req.params.id)
+    if(product){
+
+        await product.remove()
+		res.json({ message: 'Product removed' })
+    }
+    else{
+        
+		res.status(404)
+		throw new Error('Product not found')
+    }
+
+})
+router.post('/update',async(req,res)=>{
+    const product= await Product.findById(req.params.id)
+    const {
+		name,
+		price,
+		description,
+		image,
+		brand,
+		category,
+		countInStock,
+	} = req.body
+
+    if(product){
+        product.product_name = name
+		product.product_price = price
+		product.product_description = description || product.description
+		product.product_image = image || product.image
+		product.product_brand = brand || product.brand
+		product.product_category = category || product.category
+		product.product_countInStock = countInStock
+
+		const updatedProduct = await product.save()
+		res.status(201).json(updatedProduct)
+
+    }
+    else{
+        res.status(404)
+		throw new Error('Product not found')
+    }
+    router.get('/:id',async(req,res)=>{
+        const product= await Product.findById(req.params.id)
+        if(product){
+            res.json(product)
+        }
+        else{
+            res.status(404)
+		throw new Error('Product not found')
+        }
+    })
+
+})
 module.exports=router
