fc099f814a79a9f45483609b3922403525df729d
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-05-19 17:45:41 +0545

    frontend added

---
+++ frontend/components/product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Product=({Product})=>{
+    return(
+        <div>
+            <p>{Product.name}</p>
+            <img src={product.Image} /><br/>
+            <p>{product.price}</p>
+        </div>
+
+
+    )
+}
+export default  Product
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     i just want to learn more and more day by day thankyou
+   </div>
+  );
+}
+
+export default App;
